d85135223accea6b79df4628ebbcc90d29c8f2c2
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-29 02:25:24 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,21 @@
+class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    Show.maximum(:rating)
+  end
+
+  def Show::lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def Show::ratings_sum
+    Show.sum(:rating)
+  end
+
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+  end
+
+  def Show::least_popular_show
+    Show.order(rating: :asc).first
+  end 
